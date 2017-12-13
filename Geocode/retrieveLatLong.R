5cadc9842c6661f7e2aff58bb8a8d327b71babf1
Thomas Puppels <T.G.Puppels@student.tudelft.nl>
2017-12-13 02:01:37 +0100

    Add leaflet maps

---
+++ Geocode/retrieveLatLong.R
@@ -0,0 +1,18 @@
+library(ggmap)
+rm(list=ls())
+df <- read.csv("meetsOriginal.csv", check.names = FALSE)
+
+for(row in 1:nrow(df)){
+  lat <- df[row, "lat"]
+  long <- df[row, "long"]
+  if((is.na(lat)) ){
+    loc <- paste(df[row,"MeetCountry"], df[row,"MeetState"], df[row,"MeetTown"], sep = " ")
+    place <- geocode(loc, output = 'all', messaging = TRUE, override_limit = TRUE)
+    if(place$status == "OVER_QUERY_LIMIT"){
+      break
+    }
+    df[row, "lat"] <- place$results[[1]]$geometry$location$lat
+    df[row, "long"] <- place$results[[1]]$geometry$location$lng
+    write.csv(df, file = "meetsOriginal.csv",row.names = FALSE)
+  }
+}
--- PLinfoPortal/app.R
+++ PLinfoPortal/app.R
@@ -5,12 +5,52 @@ library(shinydashboard)
 library(googleVis)
 library(dplyr)
 library(readr)
+library(leaflet)
+library(rgdal)
+library(countrycode)
 
 #=================Global Variables==================================================
 
+rm(list=ls())
 ## Meet Data
 meet_data = read_csv("meets.csv")
-meet_data$LatLong = paste(meet_data$lat,meet_data$long,sep = ":")
+meet_data$MeetCountry = gsub('England','United Kingdom',meet_data$MeetCountry)
+meet_data$MeetCountry = gsub('N.Ireland','United Kingdom',meet_data$MeetCountry)
+
+# Read the file with the rgdal library in R
+
+world_spdf=readOGR( dsn= "TM_WORLD_BORDERS_SIMPL-0.3" , layer="TM_WORLD_BORDERS_SIMPL-0.3")
+
+world_spdf@data$NAME = gsub('Saint Martin', 'Malta', world_spdf@data$NAME)
+countrycodes = countrycode(world_spdf@data$NAME,'country.name','iso3c') #doesnt recognize all countries
+meetCountries = unique(meet_data$MeetCountry)
+
+for(row in 1:length(meetCountries)){ 
+  print(meetCountries[row])
+  index = match(countrycode(meetCountries[row],'country.name','iso3c'),countrycodes)
+  print(world_spdf@data$NAME[index])
+  world_spdf@data$NAME[index] = meetCountries[row]
+  print(world_spdf@data$NAME[index])
+}
+
+# for(year in min(meet_data$Year):max(meet_data$Year)){ For Leaflet precomputation
+#   dataset <- filter(meet_data, Year==year) %>% #Filters meets for the selected year
+#     count(MeetCountry) #Counts the number of meet for each country
+#   colName = 
+#   world_spdf@data[, paste("Year", year, sep="")] = 0
+#     if(nrow(dataset) != 0){
+#       for(row in 1:nrow(dataset)){
+#           print(year)
+#           print(row)
+#           
+#           index = match(dataset[[row, "MeetCountry"]], world_spdf@data$NAME)
+#           print(dataset[[row, "MeetCountry"]])
+#           print(index)
+#           world_spdf@data[index,paste("Year", year, sep="")] = dataset[row, "n"]
+#           print(world_spdf@data[,paste("Year", year, sep="")])
+#       }
+#     }
+# }
 
 #=================UI Design========================================================
 ui <- dashboardPage(skin = "green",
@@ -49,15 +89,10 @@ ui <- dashboardPage(skin = "green",
                                 fluidRow( 
                                   #row of content (full screen width is 12)
                                   
-                                  box(width = 3, title = "Dummy box", 
-                                      #dummy box which will fill 3/12 of row
-                                      p("I have no purpose.")
-                                  ),
-                                  
-                                  box(width = 9, title = "Number of meets per country",
+                                  box(width = 12, title = "Number of meets per country",
                                       #another box that will fill 9/12 of row
                                       
-                                      htmlOutput("intensity_map") 
+                                      leafletOutput("intensity_map") 
                                       #We define contents ofthis htmlOutput in server function
                                       
                                   )
@@ -67,11 +102,12 @@ ui <- dashboardPage(skin = "green",
                                   box(width = 12, title = "Number of meets per country",
                                       #another box that will fill 9/12 of row
                                       
-                                      htmlOutput("intensity_map2") 
+                                      leafletOutput("intensity_map2") 
                                       #We define contents ofthis htmlOutput in server function
                                       
                                   )
                                 )
+                                
                         ),
                         tabItem(tabName = "drugs",
                                 p("Qwerty")
@@ -100,60 +136,93 @@ server <- function(input, output) {
   
   
   
-  ## Intensity map
-  output$intensity_map <- renderGvis({
-    #It says that intensity_map on the output is to be rendered as a 
-    #google viusualization
+  # # Intensity map
+  # output$intensity_map <- renderGvis({
+  #   #It says that intensity_map on the output is to be rendered as a
+  #   #google viusualization
+  # 
+  #   #Create data that needs to be displayed
+  #   data <- filter(meet_data, Year==myYear()) %>% #Filters meets for the selected year
+  #     count(MeetCountry) #Counts the number of meet for each country
+  #   #and store it in a table called data with following format
+  # 
+  #   # MeetCountry       n
+  #   # <chr>           <int>
+  #   # 1   Australia      88
+  #   # 2     Belarus       1
+  #   # 3      Canada     108
+  #   # 4     Czechia       1
+  #   # 5     Denmark       2
+  #   # 6     England       1
+  #   # 7     Finland       5
+  #   # 8      France       3
+  #   # 9      Greece       1
+  #   # 10     Ireland      9
+  #   # 11      Israel      1
+  #   # 12  Kazakhstan      2
+  #   # 13    Malaysia      1
+  #   # 14   N.Ireland      4
+  #   # 15      Norway    100
+  #   # 16      Poland     1
+  #   # 17      Russia     6
+  #   # 18       Spain     2
+  #   # 19         USA    1096
+  # 
+  # 
+  #   #Following chart is passed to the renderGvis funciton which generates the appropriate html for output
+  #   gvisGeoChart(data, "MeetCountry", colorvar="n",
+  #                ##location is specified by value "MeetCountry" in table "data"
+  #                ##color for a location is chosen by the associated value of valiable "n" in table "data"
+  #                options=list(colorAxis = "{colors: ['#e31b23']}",
+  #                             width=800, height=500))
+  #   
+  # })
+  output$intensity_map <- renderLeaflet({
+    #colName = paste("Year", myYear(), sep="") for precomputed version
     
-    #Create data that needs to be displayed
-    data <- filter(meet_data, Year==myYear()) %>% #Filters meets for the selected year 
-      count(MeetCountry) #Counts the number of meet for each country 
-    #and store it in a table called data with following format
+    dataset <- filter(meet_data, Year==myYear()) %>% #Filters meets for the selected year
+      count(MeetCountry) #Counts the number of meet for each country
+      world_spdf@data$n = 0
+    if(nrow(dataset) != 0){
+      for(row in 1:nrow(dataset)){
+        index = match(dataset[[row, "MeetCountry"]], world_spdf@data$NAME)
+        world_spdf@data$n[index] = dataset[[row, "n"]]
+      }
+    }
+      
+    # Create a color palette with handmade bins.
+    mybins=c(0,1,10,100,200,400, 800,Inf)
+    mypalette = colorBin( palette="Oranges", domain=world_spdf@data$n, na.color="transparent", bins=mybins)
     
-    # MeetCountry       n
-    # <chr>           <int>
-    # 1   Australia      88
-    # 2     Belarus       1
-    # 3      Canada     108
-    # 4     Czechia       1
-    # 5     Denmark       2
-    # 6     England       1
-    # 7     Finland       5
-    # 8      France       3
-    # 9      Greece       1
-    # 10     Ireland      9
-    # 11      Israel      1
-    # 12  Kazakhstan      2
-    # 13    Malaysia      1
-    # 14   N.Ireland      4
-    # 15      Norway    100
-    # 16      Poland     1
-    # 17      Russia     6
-    # 18       Spain     2
-    # 19         USA    1096
+    # Prepar the text for the tooltip:
+    mytext=paste("Country: ", world_spdf@data$NAME,"<br/>", "Number of Meets: ", world_spdf@data$n, sep="") %>%
+      lapply(htmltools::HTML)
     
-    
-    #Following chart is passed to the renderGvis funciton which generates the appropriate html for output
-    gvisGeoChart(data, "MeetCountry", colorvar="n", 
-                 ##location is specified by value "MeetCountry" in table "data"
-                 ##color for a location is chosen by the associated value of valiable "n" in table "data"
-                 options=list(colorAxis = "{colors: ['#e31b23']}",
-                              width=800, height=500))
+    # Final Map
+    leaflet(world_spdf) %>% 
+      addTiles()  %>% 
+      addPolygons( 
+        fillColor = ~mypalette(n), stroke=TRUE, fillOpacity = 0.9, color="white", weight=0.3,
+        highlight = highlightOptions( weight = 5, color = ~colorNumeric("Blues", n)(n), dashArray = "", fillOpacity = 0.3, bringToFront = TRUE),
+        label = mytext,
+        labelOptions = labelOptions( style = list("font-weight" = "normal", padding = "3px 8px"), textsize = "13px", direction = "auto")
+      ) %>%
+      addLegend( pal=mypalette, values=~n, opacity=0.9, title = paste("Number of Meets in",myYear(), sep = " "), position = "bottomleft" )
   })
   
-  ## Intensity map
-  output$intensity_map2 <- renderGvis({
-    #It says that intensity_map on the output is to be rendered as a 
-    #google viusualization
-    
+  output$intensity_map2 <- renderLeaflet({
     #Create data that needs to be displayed
-    data <- filter(meet_data, Year==myYear()) 
-    
+    meets <- filter(meet_data, Year==myYear())
     
     #Following chart is passed to the renderGvis funciton which generates the appropriate html for output
-    gvisGeoChart(data, "LatLong", colorvar = "NumberParticipants", hovervar = "MeetName",
-                 options=list(colorAxis = "{colors: ['#e31b23']}",
-                width=800, height=500, magnifyingGlass = "{enable: true, zoomFactor: 7.5}"))
+    leaflet(data = meets) %>% 
+      addTiles() %>%
+      addMarkers(~long, ~lat, popup = ~as.character(NumberParticipants), label =~as.character(MeetName) )
+      # addPolygons(fillColor = ~pal(gdp08), 
+      #             fillOpacity = 0.8, 
+      #             color = "#BDBDC3", 
+      #             weight = 1)
+    
   })
 }
 
